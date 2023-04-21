d5f7f3a1a5cb0285c4e1bd300042d32a8dee97a5
Shawnice <wucean@gmail.com>
2023-04-21 08:55:33 +0100

    refactor: move logging settings

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# Standard library
+import logging
+import sys
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
